93994659647ba4c38b3f51698cc1a99bb9fecd92
ScenesK <scenesk.ngt@gmail.com>
2020-05-23 00:34:59 +0900

    Switch to jupyterlab in hypothesis testing

--- workspace/intermediate/my_functions/hypothesis_testing/one_sided.py
+++ workspace/intermediate/my_functions/hypothesis_testing/one_sided.py
@@ -14,39 +14,43 @@ def plot(alpha):
     left = np.linspace(x_min, range_left, resolution)
     center = np.linspace(range_left, range_right, resolution)
     right = np.linspace(range_right, x_max, resolution)
-    _, axes = plt.subplots(
-        1, 2, figsize=figaspect(1 / 2), sharex=True, sharey=True)
+    _, axes = plt.subplots(1,
+                           2,
+                           figsize=figaspect(1 / 2),
+                           sharex=True,
+                           sharey=True)
     for i, ax in enumerate(axes):
-        ax.fill_between(
-            center, distribution.pdf(center), color='blue', label='採択域')
-        if i is 0:
+        ax.fill_between(center,
+                        distribution.pdf(center),
+                        color='blue',
+                        label='採択域')
+        if i == 0:
             rejection = left
             title = '左側検定'
         else:
             rejection = right
             title = '右側検定'
-        ax.fill_between(
-            rejection, distribution.pdf(rejection), color='red', label='棄却域')
+        ax.fill_between(rejection,
+                        distribution.pdf(rejection),
+                        color='red',
+                        label='棄却域')
         acception = -rejection
         ax.fill_between(acception, distribution.pdf(acception), color='blue')
         ax.legend()
-        ax.set(
-            title=title,
-            xlim=(x_min, x_max),
-            ylim=(0, ax.get_ylim()[1]),
-            xticks=(),
-            yticks=())
+        ax.set(title=title,
+               xlim=(x_min, x_max),
+               ylim=(0, ax.get_ylim()[1]),
+               xticks=(),
+               yticks=())
     plt.show()
 
 
 def show():
-    interact(
-        plot,
-        alpha=FloatSlider(
-            value=0.05,
-            min=0.01,
-            max=0.3,
-            step=0.01,
-            description='有意水準',
-            continuous_update=False,
-            readout_format='.2f'))
+    interact(plot,
+             alpha=FloatSlider(value=0.05,
+                               min=0.01,
+                               max=0.3,
+                               step=0.01,
+                               description='有意水準',
+                               continuous_update=False,
+                               readout_format='.2f'))
